c38b1da4fe063ed0f7c6e2206df853c6468ba66e
Rishabh Kumar <70692082+kr12rishabh@users.noreply.github.com>
2022-09-03 12:10:56 +0530

    yes

--- script.js
+++ script.js
@@ -85,7 +85,7 @@ btnHold.addEventListener('click', function () {
       scores[activePlayer];
 
     //2.check score is already >=100
-    if (scores[activePlayer] >= 15) {
+    if (scores[activePlayer] >= 100) {
       playing = false;
       diceEl.classList.add('hidden');
 
